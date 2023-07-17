deb4a9e765545854522263aafa23c787286ca7b6
d.m <dmapp222@gmail.com>
2023-07-18 04:42:10 +0900

    表示コメントの変更

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              '表示コメントの変更',
+              '表示コメントの変更したよ',
             ),
             Text(
               '$_counter',
